51468852945ed832210c6aaccadadcd6c6b48259
vsantidev <vsantidev@gmail.com>
2023-11-08 14:33:59 +0100

    modif link navbar

--- index.php
+++ index.php
@@ -16,14 +16,14 @@
     <header>
         <nav class="navbar navbar-expand-lg p_bgGreen">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">Vans</a>
+                <a class="navbar-brand" href="index.php">Vans</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="./views/form_product.php">Enchères</a>
+                            <a class="nav-link active" aria-current="page" href="product.php">Enchères</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="#">Historique</a>
