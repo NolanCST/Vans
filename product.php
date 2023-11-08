4a33742bd46e385053d4162eef4e7ef2793f6e9a
AndreaBGLR <andrea.baglieri@laposte.net>
2023-11-08 16:56:35 +0100

    correction

--- product.php
+++ product.php
@@ -6,9 +6,8 @@ require __DIR__."/classes/products.class.php";
 
 
 if($_SERVER["REQUEST_METHOD"] == "POST") {
-    $myProduct = new Produits (
-        $_POST["starting_price"], 
-        $_POST["last_price"], 
+    $myProduct = new Product (
+        $_POST["starting_price"],
         $_POST["end_date"], 
          $_POST["model"] ,
          $_POST["mark"] ,
@@ -16,29 +15,29 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
          $_POST["year"] ,
          $_POST["description"] 
         );
-      $myProduct -> sauvegarde();
+      $myProduct -> save();
     }
 
 
-$dbh = new PDO("mysql:dbname=vans;host=127.0.0.1", "root", "");
+// $dbh = new PDO("mysql:dbname=vans;host=127.0.0.1", "root", "");
 
-$query = $dbh->prepare("SELECT * FROM product");
-
-
-$query->execute();
+// $query = $dbh->prepare("SELECT * FROM product");
 
 
-$results = $query->fetchAll();
+// $query->execute();
 
-foreach ($results as $result) {
-    echo "<h1> N° $result[id]: Starting price : $result[starting_price]</h1>";
-    echo "<h1> Last price:  $result[last_price]</h1>";
-    echo "<h1> end_date : $result[end_date]</h1>";
-    echo "<h1> model : $result[model]</h1>";
-    echo "<h1> mark : $result[mark]</h1>";
-    echo "<h1> power : $result[power]</h1>";
-    echo "<h1> year : $result[year]</h1>";
-    echo "<h1> description : $result[description]</h1>";
-    echo "<h1> ___________________</h1>";
-}
+
+// $results = $query->fetchAll();
+
+// foreach ($results as $result) {
+//     echo "<h1> N° $result[id]: Starting price : $result[starting_price]</h1>";
+//     echo "<h1> Last price:  $result[last_price]</h1>";
+//     echo "<h1> end_date : $result[end_date]</h1>";
+//     echo "<h1> model : $result[model]</h1>";
+//     echo "<h1> mark : $result[mark]</h1>";
+//     echo "<h1> power : $result[power]</h1>";
+//     echo "<h1> year : $result[year]</h1>";
+//     echo "<h1> description : $result[description]</h1>";
+//     echo "<h1> ___________________</h1>";
+// }
 
--- views/form_product.php
+++ views/form_product.php
@@ -1,13 +1,10 @@
-<?php 
-
- echo "<form   action=\"product.php\" method=\"POST\"  >";
- echo "<input name=\"starting_price\" placeholder=\"Starting Price\" type=\"number\"> </input><br/>";
- echo "<input name=\"last_price\" placeholder=\"Lastprice\" type=\"number\"> </input><br/>";
- echo "<input name=\"end_date\" placeholder=\"end date\" type=\"date\"> </input><br/>";
- echo "<input name=\"model\" placeholder=\"model\" type=\"text\"> </input><br/>";
- echo "<input name=\"mark\" placeholder=\"mark\" type=\"text\"> </input><br/>";
- echo "<input name=\"power\" placeholder=\"power\" type=\"text\"> </input><br/>";
- echo "<input name=\"year\" placeholder=\"year\" type=\"number\"> </input><br/>";
- echo "<input name=\"description\" placeholder=\"description\" type=\"text\"> </input><br/>";
- echo "<input type=\"submit\" value= \"Good\"> </input><br/>";
- echo "</form>";
+<form   action="product.php" method="POST"  >
+    <input name="starting_price" placeholder="Starting Price" type="number"> </input><br/>
+    <input name="end_date" placeholder="end date" type="date"> </input><br/>
+    <input name="model" placeholder="model" type="text"> </input><br/>
+    <input name="mark" placeholder="mark" type="text"> </input><br/>
+    <input name="power" placeholder="power" type="text"> </input><br/>
+    <input name="year" placeholder="year" type="number"> </input><br/>
+    <input name="description" placeholder="description" type="text"> </input><br/>
+    <input type="submit" value= "Good"> </input><br/>
+</form>
