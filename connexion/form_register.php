de8aec5b158c20cef1c75dd5e21424c9c365b93c
NolanCST <nolan.costa06@gmail.com>
2023-11-11 00:30:05 +0100

    reorganisation du css de connexion

--- connexion/form_register.php
+++ connexion/form_register.php
@@ -1,10 +1,10 @@
-<form class="box" action="register.php" method="post" name="register">
-    <h1 class="box-title">S'inscrire</h1>
-    <input type="text" class="box-input" name="lastname" placeholder="Nom" maxlength="25" required />
-    <input type="text" class="box-input" name="firstname" placeholder="Prenom" maxlength="25" required />
-    <input type="text" class="box-input" name="email" placeholder="Email" required />
-    <input type="password" class="box-input" name="password" placeholder="Mot de passe*" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" required />
+<form class="connexionContainer" action="register.php" method="post" name="register">
+    <h1 class="connexionTitle">S'inscrire</h1>
+    <input type="text" class="connexionInput" name="lastname" placeholder="Nom" maxlength="25" required />
+    <input type="text" class="connexionInput" name="firstname" placeholder="Prenom" maxlength="25" required />
+    <input type="text" class="connexionInput" name="email" placeholder="Email" required />
+    <input type="password" class="connexionInput" name="password" placeholder="Mot de passe*" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" required />
     <p class="textPassword">*Au moins 8 caractères, un chiffre, une lettre majuscule et une minuscule.</p>
-    <input type="submit" name="submit" value="S'inscrire" class="box-button" />
+    <input type="submit" name="submit" value="S'inscrire" class="connexionButton" />
     <p class="box-register">Déjà inscrit? <a href="login.php">Connectez-vous ici</a></p>
 </form>
--- views/navigation.php
+++ views/navigation.php
@@ -4,7 +4,7 @@
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <link rel="stylesheet" type="text/css" href="product.css" />
-            <!-- <link rel="stylesheet" type="text/css" href="connexion.css" /> -->
+            <link rel="stylesheet" type="text/css" href="connexion.css" />
             <link rel="stylesheet" type="text/css" href="style.css" />
             <link rel="stylesheet" type="text/css" href="profil.css" />
             <link rel="preconnect" href="https://fonts.googleapis.com">
