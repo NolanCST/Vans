85bda04688b237c8caacff32c41cdff76fff0821
AndreaBGLR <andrea.baglieri@laposte.net>
2023-11-10 13:51:26 +0100

    css

--- views/details_product.php
+++ views/details_product.php
@@ -3,8 +3,8 @@
         <head>
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <link rel="stylesheet" type="text/css" href="product.css" />
-            <link rel="stylesheet" type="text/css" href="style.css" />
+            <link rel="stylesheet" type="text/css" href="details_product.css" />
+            <link rel="stylesheet" type="text/css" href="../style.css" />
             <link rel="preconnect" href="https://fonts.googleapis.com">
             <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
             <link href="https://fonts.googleapis.com/css2?family=Ms+Madi&family=Raleway:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300&display=swap" rel="stylesheet">
@@ -14,38 +14,44 @@
         </head>
 
         <body>
-            <header>
+            <!-- <header>
                 <?php
                 include __DIR__ . "/layouts/navigation.php";
                 ?>
-            </header>
+            </header> -->
             <section>
+                
                 <?php
-                require __DIR__ . "/views/form_product.php";
-                require __DIR__ . "/classes/products.class.php";
-                if ($_SERVER["REQUEST_METHOD"] == "POST") {
-                    $myProduct = new Product(
+$dbh = new PDO("mysql:dbname=vans;host=127.0.0.1", "root", "");
 
-                        $_POST["title"],
-                        $_POST["mark"],
-                        $_POST["model"],
-                        $_POST["power"],
-                        $_POST["year"],
-                        $_POST["description"],
-                        $_POST["starting_price"],
-                        $_POST["end_date"],
-                    );
-                    $myProduct->save();
-                }
-                // var_dump($myProduct);
-                // die();
-            ?>
-              
-            </section>
-            <footer>
-                <?php
-                // require __DIR__ . "/layouts/footer.php";
-                ?>
-            </footer>
-        </body>
-    </html>
+$query = $dbh->prepare("SELECT * FROM `product` p LEFT JOIN `details` d ON p.id_product = d.id_product");
+
+
+$query->execute();
+
+
+$results = $query->fetchAll();
+
+foreach ($results as $result) {
+    echo "<div class=\"dtails_box\">";
+    echo "<h1> Informations complementaires concernant l'annonce :<br></h1>";
+    echo "<h2> $result[title]</h2>"; 
+    echo "<p> Marque : $result[mark]</p>";
+    echo "<p> Modèle : $result[model]</p>";
+    echo "<p> Puissance : $result[power]</p>";
+    echo "<p> Année : $result[year]</p>";
+    echo "<p> Description : $result[description]</p>";
+    echo "<p> Prix de départ : $result[starting_price]</p>";
+    echo "<p> Date de fin : $result[end_date]</p>";
+    echo "</div>";
+}
+       ?>   
+      
+</section>
+<footer>
+    <?php
+    // require __DIR__ . "/layouts/footer.php";
+    ?>
+</footer>
+</body>
+</html>
