72ab6581629c61fa409cc6304293b2c9251187c8
NolanCST <nolan.costa06@gmail.com>
2023-11-13 14:54:07 +0100

    Supprssion trucs inutiles

--- dataBase.php
+++ dataBase.php
@@ -1,10 +1,4 @@
 <?php
-// windows
-/* $dbh = new PDO("mysql:dbname=vans;host=127.0.0.1", "root", ""); */
-
-// Mac
-/*  $dbh = new PDO("mysql:dbname=vans;host=127.0.0.1; port=8889", "root", "root"); */
-
     try{
         //windows
         $dbh = new PDO("mysql:dbname=vans;host=127.0.0.1", "root", "");
