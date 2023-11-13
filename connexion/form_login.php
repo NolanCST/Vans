010eb9ef39f96f4b3d2c906664d1d8e9385d976e
vsantidev <vsantidev@gmail.com>
2023-11-13 11:12:46 +0100

    modif css connexion et inscription 2

--- connexion/form_login.php
+++ connexion/form_login.php
@@ -1,28 +1,19 @@
-
-<section class="box">
-<form class="form-floating" action="login.php" method="post" name="login">
-    <h1 class="box-title">Connexion</h1>
-    <!-- <div class="form-group">
-        <input type="email" class="box-input" name="email" placeholder="Email" required>
-    </div>
-    <div class="form-group">
-        <input type="password" class="box-input" name="password" placeholder="Mot de passe" required>
-    </div> -->
-    <div class="form-floating mb-3">
-        <input type="email" class="form-control transparent-input" id="floatingInput" name="email" placeholder="name@example.com">
-        <label for="floatingInput">Adresse email</label>
-    </div>
-    <div class="form-floating">
-        <input type="password" class="form-control transparent-input" id="floatingPassword" name="password" placeholder="Password">
-        <label for="floatingPassword">Mot de passe</label>
-    </div>
-    <div class="form-group">
-        <input type="submit" value="Connexion " name="submit" class="btn btn-login btn-warning">
-    </div>
-    <div class="form-group">
-        <p class="box-register">Vous êtes nouveau ici? <a href="register.php">S'inscrire</a></p>
-    </div>
-
-
-</form>
+<section class="connexionContainer">
+    <form class="form-floating" action="login.php" method="post" name="login">
+        <h1 class="box-title">Connexion</h1>
+        <div class="form-floating mb-3">
+            <input type="email" class="form-control transparent-input" id="floatingInput" name="email" placeholder="name@example.com">
+            <label for="floatingInput">Adresse email</label>
+        </div>
+        <div class="form-floating">
+            <input type="password" class="form-control transparent-input" id="floatingPassword" name="password" placeholder="Password">
+            <label for="floatingPassword">Mot de passe</label>
+        </div>
+        <div class="form-group">
+            <input type="submit" value="Connexion " name="submit" class="btn btn-login btn-warning">
+        </div>
+        <div class="form-group">
+            <p class="box-register">Vous êtes nouveau ici? <a href="register.php">S'inscrire</a></p>
+        </div>
+    </form>
 </section>
--- connexion/form_register.php
+++ connexion/form_register.php
@@ -1,10 +1,14 @@
 <form class="connexionContainer" action="register.php" method="post" name="register">
-    <h1 class="connexionTitle">S'inscrire</h1>
+    <h1 class="box-title">Inscription</h1>
     <input type="text" class="connexionInput" name="lastname" placeholder="Nom" maxlength="25" required />
     <input type="text" class="connexionInput" name="firstname" placeholder="Prenom" maxlength="25" required />
     <input type="text" class="connexionInput" name="email" placeholder="Email" required />
     <input type="password" class="connexionInput" name="password" placeholder="Mot de passe*" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*_=+]).{12,}" required />
     <p class="textPassword">*Au moins 12 caractères, un chiffre, une lettre majuscule, une minuscule, un caractère parmi !@#$%^&*_=+.</p>
-    <input type="submit" name="submit" value="S'inscrire" class="connexionButton" />
-    <p class="box-register">Déjà inscrit? <a href="login.php">Connectez-vous ici</a></p>
+    <div class="form-group">
+        <input type="submit" value="S'inscrire" name="submit" class="btn btn-login btn-warning">
+    </div>
+    <div class="form-group">
+        <p class="box-register">Déjà inscrit?<a href="login.php">Connectez-vous ici</a></p>
+    </div>
 </form>
