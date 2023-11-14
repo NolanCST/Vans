cbdcacd6d944aa789562930304a5523dd0ba0d84
vsantidev <vsantidev@gmail.com>
2023-11-14 14:21:55 +0100

    rajout bouton reloadr

--- views/navigation.php
+++ views/navigation.php
@@ -23,7 +23,7 @@
                 <nav class="navbar navbar-expand-lg bg-body-tertiary fixed-top p_bgGreen" data-bs-theme="dark">
                   <div class="container-fluid">
                     <!-- <a class="navbar-brand p_ftLogo" href="home.php">Vans</a> -->
-                    <a class="navbar-brand p_ftLogo" href="../images/a.png">Vans</a>
+                    <a class="navbar-brand p_ftLogo" href="home.php">Vans</a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                       <span class="navbar-toggler-icon"></span>
                     </button>
