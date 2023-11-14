e0027393ac468cb284e4c8aba06408c2fba6d0c8
sergioMG98 <sergio.marquesgomes98@gmail.com>
2023-11-14 16:56:41 +0100

    lien footer

--- views/sous_dossier_footer/qui_sommes_nous.php
+++ views/sous_dossier_footer/qui_sommes_nous.php
@@ -4,12 +4,83 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="s.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </head>
 <body>
-    <header>
+    <header class="header">
         <h1>qui sommes nous ?</h1>
+        <div class="presentation">
+            <h2>Nous sommes des spartiate</h2>
+            <p>non je plaisante ;) nous sommes un groupe de personnes en reconvertion professionnelle afin de devenir des développeur web full stack </p>
+            <p>se site à été conçu par 4 personnes que voici :</p>
+        </div>
 
-        
     </header>
+
+    <section class="section">
+
+        <div class="card mb-3 andrea" style="max-width: 80%;">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="../../images/a.png" class="img-fluid rounded-start" alt="...">
+                </div>
+
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title dev">andrea</h5>
+                        <p class="card-text">la gloutonne comme vous pouvez le voir dans l'image elle est prete a manger . PS : elle a un garde manger planqué dans le meuble kallax uniquement pour elle.</p>
+                        <!-- <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p> -->
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card mb-3 nolan" style="max-width: 80%;">
+            <div class="row g-0">
+                <div class="col-md-4 nolanus">
+                    <img src="../../images/n.png" class="img-fluid rounded-start" alt="...">
+                </div>
+
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title dev">nolan</h5>
+                        <p class="card-text">surnommé "nolanus" ou encore "nathan le tyran" on vous laisse imaginé pourquoi</p>
+                        <!-- <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p> -->
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card mb-3 veronique" style="max-width: 80%;">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="../../images/v.png" class="img-fluid rounded-start" alt="...">
+                </div>
+
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title dev">veronique</h5>
+                        <p class="card-text">veronique </p>
+                        <!-- <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p> -->
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card mb-3 sergio" style="max-width: 80%;">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="../../images/d.gif" class="img-fluid rounded-start" alt="...">
+                </div>
+
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title dev">sergio</h5>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <!-- <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p> -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 </body>
 </html>
